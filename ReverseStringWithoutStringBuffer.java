10fcaa11dd77b217d3716a73a5cf99df23f4d1d2
Farhan Laeeq <far619@gmail.com>
2016-08-05 12:29:47 +0530

    Reverse String without using StringBuffer

---
+++ ReverseStringWithoutStringBuffer.java
@@ -0,0 +1,21 @@
+package javabeginnerprograms;
+
+public class ReverseStringWithoutStringBuffer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="github";
+		char[] arr=str.toCharArray();
+		for(int i=0;i<str.length()/2;i++){
+			char temp=arr[i];
+			arr[i]=arr[str.length()-1-i];
+			arr[str.length()-1-i]=temp;
+		}	
+		String rev="";
+		for(int i=0;i<arr.length;i++)
+			rev+=arr[i];
+		System.out.println("Original String :- "+str);
+		System.out.println("Reverse String :- "+rev);
+	}
+
+}
